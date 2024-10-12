aa7f9913acedcdd52d511b554fc981e0b8bc09cc
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2024-10-12 14:02:34 +0700

    Quartz sync: Oct 12, 2024, 2:02 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,10 +32,10 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph({
       localGraph: {
-        drag: true, // whether to allow panning the view around
-        zoom: true, // whether to allow zooming in and out
-        depth: 1, // how many hops of notes to display
-        scale: 1.1, // default view scale
+        drag: true,
+        zoom: true,
+        depth: -1,
+        scale: 0.9,
         repelForce: 20, // how much nodes should repel each other
         centerForce: 0.7, // how much force to use when trying to center the nodes
         linkDistance: 100, // how long should the links be by default?
@@ -45,10 +45,10 @@ export const defaultContentPageLayout: PageLayout = {
         showTags: true, // whether to show tags in the graph
       },
       globalGraph: {
-        drag: true, // whether to allow panning the view around
-        zoom: true, // whether to allow zooming in and out
-        depth: 1, // how many hops of notes to display
-        scale: 1.1, // default view scale
+        drag: true,
+        zoom: true,
+        depth: -1,
+        scale: 0.9,
         repelForce: 20, // how much nodes should repel each other
         centerForce: 0.7, // how much force to use when trying to center the nodes
         linkDistance: 100, // how long should the links be by default?
