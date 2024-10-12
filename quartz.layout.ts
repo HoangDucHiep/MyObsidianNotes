2b4e1c2f2ea32fdcafd4382176d5617389451e62
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2024-10-12 14:03:54 +0700

    Quartz sync: Oct 12, 2024, 2:03 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,13 +32,13 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph({
       localGraph: {
-        drag: true,
-        zoom: true,
-        depth: -1,
-        scale: 0.9,
-        repelForce: 20, // how much nodes should repel each other
-        centerForce: 0.7, // how much force to use when trying to center the nodes
-        linkDistance: 100, // how long should the links be by default?
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 1, // how many hops of notes to display
+        scale: 1.1, // default view scale
+        repelForce: 0.5, // how much nodes should repel each other
+        centerForce: 0.3, // how much force to use when trying to center the nodes
+        linkDistance: 30, // how long should the links be by default?
         fontSize: 0.6, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
@@ -49,11 +49,11 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true,
         depth: -1,
         scale: 0.9,
-        repelForce: 20, // how much nodes should repel each other
-        centerForce: 0.7, // how much force to use when trying to center the nodes
-        linkDistance: 100, // how long should the links be by default?
-        fontSize: 0.6, // what size should the node labels be?
-        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+        repelForce: 0.5,
+        centerForce: 0.3,
+        linkDistance: 30,
+        fontSize: 0.6,
+        opacityScale: 1,
         removeTags: [], // what tags to remove from the graph
         showTags: true, // whether to show tags in the graph
       },
